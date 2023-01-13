cc65bfc1a6642bdb916d896b0788105c43c65c9c
Amit Debnath <amit@technicise.online>
2023-01-14 02:16:09 +0530

    Modifying main to incorporate GCP

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"time"
+
 	// "sync"
 	"v2/model"
 
@@ -13,6 +15,9 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"google.golang.org/api/iterator"
+
+	"cloud.google.com/go/storage"
 )
 
 type AwsAdapter struct {
@@ -21,19 +26,21 @@ type AwsAdapter struct {
 
 func main() {
 
-    sd, _ := CreateDriver("ap-south-1")
-    buckArr, _ := sd.BucketList(context.TODO())
+	sd, _ := CreateDriver("ap-south-1")
+	client, ctx1, _ := CreateDriverGCP("ap-south-1")
+	buckArr, _ := sd.BucketList(context.TODO())
+	BucketListGCP(client, ctx1)
 	fmt.Print(len(buckArr))
 	out, _ := json.MarshalIndent(buckArr, "", "   ")
-    err := ioutil.WriteFile("metadata.json", out, 0644)
-    if err != nil {
-        log.Fatal(err)
-    }
+	err := ioutil.WriteFile("metadata.json", out, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func CreateDriver(region string) (*AwsAdapter, error) {
-	AKID := "your_access_key"
-	SECRET_KEY := "your_secret_key"
+	AKID := "AKIAVXJPFJZ62DWFG77U"
+	SECRET_KEY := "O8vw+HBDws5SgEMETDiM3A+ujcKZZlUVeqwZ8pQ4"
 	creds := credentials.NewStaticCredentials(AKID, SECRET_KEY, "")
 
 	sess, err := session.NewSession(&aws.Config{
@@ -47,6 +54,66 @@ func CreateDriver(region string) (*AwsAdapter, error) {
 	adap := &AwsAdapter{Session: sess}
 
 	return adap, nil
+}
+
+func CreateDriverGCP(region string) (*storage.Client, context.Context, error) {
+	// AKID := "AKIAVXJPFJZ62DWFG77U"
+	// SECRET_KEY := "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"
+	// creds := credentials.NewStaticCredentials(AKID, SECRET_KEY, "")
+
+	ctx1 := context.Background()
+        client, err := storage.NewClient(ctx1)
+        if err != nil {
+			return nil, nil, fmt.Errorf("storage.NewClient: %v", err)
+		}
+		defer client.Close()
+
+        ctx1, cancel := context.WithTimeout(ctx1, time.Second*30)
+        defer cancel()
+
+	return client, ctx1, nil
+}
+
+func BucketListGCP(gcc *storage.Client, ctx context.Context) ([]model.MetaBucket, error) {
+	
+	projectID := "my-project-id"
+	var err error
+	bucketArray := make([]model.MetaBucket, 0)
+        it := gcc.Buckets(ctx, projectID)
+        for {
+			bucket, err := it.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+					return nil, err
+			}
+			buck := model.MetaBucket{}
+			buck.CreationDate = &bucket.Created
+			buck.Name = bucket.Name
+
+			// loc, _ := svc.GetBucketLocation(&s3.GetBucketLocationInput{Bucket: bucket.Name})
+			// buck.Region = *loc.LocationConstraint
+			// ad.Session.Config.Region = loc.LocationConstraint
+			// svc = s3.New(ad.Session)
+			// tags, _ := svc.GetBucketTagging(&s3.GetBucketTaggingInput{Bucket: bucket.Name})
+			// tagset := make(map[string]string)
+			// for _, tag := range tags.TagSet {
+			// 	tagset[*tag.Key] = *tag.Value
+			// }
+			// buck.BucketTags = tagset
+			// objects, err := ad.ObjectList(ctx, &buck)
+			// buck.Objects = objects
+			if err == nil {
+				// lock.Lock()
+				// defer lock.Unlock()
+				bucketArray = append(bucketArray, buck)
+			}
+        }
+        
+	// wg.Wait()
+	 
+	return bucketArray, err
 }
 
 func (ad *AwsAdapter) BucketList(ctx context.Context) ([]model.MetaBucket, error) {
@@ -73,7 +140,7 @@ func (ad *AwsAdapter) BucketList(ctx context.Context) ([]model.MetaBucket, error
 		tagset := make(map[string]string)
 		for _, tag := range tags.TagSet {
 			tagset[*tag.Key] = *tag.Value
-		} 
+		}
 		buck.BucketTags = tagset
 		objects, err := ad.ObjectList(ctx, &buck)
 		buck.Objects = objects
@@ -92,7 +159,7 @@ func (ad *AwsAdapter) ObjectList(ctx context.Context, bucket *model.MetaBucket) 
 
 	output, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: &bucket.Name})
 	if err != nil {
-        return nil, err
+		return nil, err
 	}
 	objectArray := make([]model.MetaObject, 0)
 	// var wg sync.WaitGroup
@@ -107,13 +174,21 @@ func (ad *AwsAdapter) ObjectList(ctx context.Context, bucket *model.MetaBucket) 
 		obj.Size = *object.Size
 		totSize += obj.Size
 		obj.StorageClass = *object.StorageClass
-		
+
 		meta, _ := svc.HeadObject(&s3.HeadObjectInput{Bucket: &bucket.Name, Key: object.Key})
-		if meta.ServerSideEncryption != nil {obj.ServerSideEncryption = *meta.ServerSideEncryption}
-		if meta.VersionId != nil {obj.VersionId = *meta.VersionId}
-		obj.ObjectType = *meta.ContentType 
-		if meta.Expires != nil {obj.ExpiresDate = *meta.Expires}
-		if meta.ReplicationStatus != nil {obj.ReplicationStatus = *meta.ReplicationStatus}
+		if meta.ServerSideEncryption != nil {
+			obj.ServerSideEncryption = *meta.ServerSideEncryption
+		}
+		if meta.VersionId != nil {
+			obj.VersionId = *meta.VersionId
+		}
+		obj.ObjectType = *meta.ContentType
+		if meta.Expires != nil {
+			obj.ExpiresDate = *meta.Expires
+		}
+		if meta.ReplicationStatus != nil {
+			obj.ReplicationStatus = *meta.ReplicationStatus
+		}
 		// lock.Lock()
 		// defer lock.Unlock()
 		objectArray = append(objectArray, obj)
@@ -124,4 +199,3 @@ func (ad *AwsAdapter) ObjectList(ctx context.Context, bucket *model.MetaBucket) 
 	bucket.TotalSize = totSize
 	return objectArray, err
 }
-
