f8c800e3cd6e4cf06f3c800ebc066245e79ea90d
Amit Debnath <amit@technicise.online>
2023-01-26 19:07:54 +0530

    GetBuckets and get objects trial

--- main.go
+++ main.go
@@ -6,15 +6,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"time"
 
 	// "sync"
 	"v2/model"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"google.golang.org/api/iterator"
 
 	"cloud.google.com/go/storage"
@@ -26,176 +22,145 @@ type AwsAdapter struct {
 
 func main() {
 
-	sd, _ := CreateDriver("ap-south-1")
-	client, ctx1, _ := CreateDriverGCP("ap-south-1")
-	buckArr, _ := sd.BucketList(context.TODO())
-	BucketListGCP(client, ctx1)
-	fmt.Print(len(buckArr))
-	out, _ := json.MarshalIndent(buckArr, "", "   ")
+	client, ctx1, _ := CreateDriverGCP()
+	buckArr1, _ := BucketListGCP(&client, ctx1, "640353178356")
+	fmt.Print("AMIT:::")
+	fmt.Print(len(buckArr1))
+
+	out, _ := json.MarshalIndent(buckArr1, "", "   ")
 	err := ioutil.WriteFile("metadata.json", out, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func CreateDriver(region string) (*AwsAdapter, error) {
-	AKID := "AKIAVXJPFJZ62DWFG77U"
-	SECRET_KEY := "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"
-	creds := credentials.NewStaticCredentials(AKID, SECRET_KEY, "")
-
-	sess, err := session.NewSession(&aws.Config{
-		Region:      &region,
-		Credentials: creds,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	adap := &AwsAdapter{Session: sess}
-
-	return adap, nil
-}
-
-func CreateDriverGCP(region string) (*storage.Client, context.Context, error) {
-	// AKID := "AKIAVXJPFJZ62DWFG77U"
-	// SECRET_KEY := "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"
-	// creds := credentials.NewStaticCredentials(AKID, SECRET_KEY, "")
-
+func CreateDriverGCP() (storage.Client, context.Context, error) {
 	ctx1 := context.Background()
-        client, err := storage.NewClient(ctx1)
-        if err != nil {
-			return nil, nil, fmt.Errorf("storage.NewClient: %v", err)
-		}
-		defer client.Close()
+	client, err := storage.NewClient(ctx1)
+	if err != nil {
+		return *client, nil, fmt.Errorf("storage.NewClient: %v", err)
+	}
+	defer client.Close()
 
-        ctx1, cancel := context.WithTimeout(ctx1, time.Second*30)
-        defer cancel()
-
-	return client, ctx1, nil
+	return *client, ctx1, nil
 }
 
-func BucketListGCP(gcc *storage.Client, ctx context.Context) ([]model.MetaBucket, error) {
-	
-	projectID := "my-project-id"
+func BucketListGCP(gcc *storage.Client, ctx context.Context, projectNumber string) ([]model.MetaBucket, error) {
+
 	var err error
 	bucketArray := make([]model.MetaBucket, 0)
-        it := gcc.Buckets(ctx, projectID)
-        for {
-			bucket, err := it.Next()
-			if err == iterator.Done {
-				break
-			}
-			if err != nil {
-					return nil, err
-			}
-			buck := model.MetaBucket{}
-			buck.CreationDate = &bucket.Created
-			buck.Name = bucket.Name
+	fmt.Printf("gcc: %v\n", gcc)
+	it := gcc.Buckets(ctx, projectNumber)
+	fmt.Printf("it: %v\n", it)
+	i := 0
+	for {
+		bucket, err := it.Next()
+		i = i + 1
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		go iterateBuckets(i, gcc, ctx, projectNumber, bucket, bucketArray)
 
-			// loc, _ := svc.GetBucketLocation(&s3.GetBucketLocationInput{Bucket: bucket.Name})
-			// buck.Region = *loc.LocationConstraint
-			// ad.Session.Config.Region = loc.LocationConstraint
-			// svc = s3.New(ad.Session)
-			// tags, _ := svc.GetBucketTagging(&s3.GetBucketTaggingInput{Bucket: bucket.Name})
-			// tagset := make(map[string]string)
-			// for _, tag := range tags.TagSet {
-			// 	tagset[*tag.Key] = *tag.Value
-			// }
-			// buck.BucketTags = tagset
-			// objects, err := ad.ObjectList(ctx, &buck)
-			// buck.Objects = objects
-			if err == nil {
-				// lock.Lock()
-				// defer lock.Unlock()
-				bucketArray = append(bucketArray, buck)
-			}
-        }
-        
+		fmt.Print("AMIT:::BUCKET Name " + bucket.Name + "\n")
+		fmt.Printf("bucket: %v\n", bucket)
+
+	}
+
 	// wg.Wait()
-	 
+
 	return bucketArray, err
 }
 
-func (ad *AwsAdapter) BucketList(ctx context.Context) ([]model.MetaBucket, error) {
-	svc := s3.New(ad.Session)
+// func iterateObjects(i int, object *s3.Object, svc *s3.S3, bucketName string, objectArray []model.MetaObject, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	obj := model.MetaObject{}
+// 	obj.LastModifiedDate = *object.LastModified
+// 	obj.ObjectName = *object.Key
+// 	obj.Size = *object.Size
+// 	obj.StorageClass = *object.StorageClass
 
-	output, err := svc.ListBuckets(&s3.ListBucketsInput{})
+// 	meta, err := svc.HeadObject(&s3.HeadObjectInput{Bucket: &bucketName, Key: object.Key})
+// 	if err != nil {
+// 		log.Errorf("cannot perform head object on object %v in bucket %v. failed with error: %v\n", *object.Key, bucketName, err)
+// 	}
+// 	if meta.ServerSideEncryption != nil {
+// 		obj.ServerSideEncryption = *meta.ServerSideEncryption
+// 	}
+// 	if meta.VersionId != nil {
+// 		obj.VersionId = *meta.VersionId
+// 	}
+// 	obj.ObjectType = *meta.ContentType
+// 	if meta.Expires != nil {
+// 		expiresTime, err := time.Parse(time.RFC3339, *meta.Expires)
+// 		if err != nil {
+// 			log.Errorf("unable to parse given string to time type. error: %v. skipping ExpiresDate field\n", err)
+// 		} else {
+// 			obj.ExpiresDate = expiresTime
+// 		}
+// 	}
+// 	if meta.ReplicationStatus != nil {
+// 		obj.ReplicationStatus = *meta.ReplicationStatus
+// 	}
+// 	objectArray[i] = obj
+// }
 
-	if err != nil {
-		log.Fatal(err)
-	}
-	bucketArray := make([]model.MetaBucket, 0)
-	// var wg sync.WaitGroup
-	// var lock sync.Mutex
-	for _, bucket := range output.Buckets {
-		// wg.Add(1)
-		buck := model.MetaBucket{}
-		buck.CreationDate = bucket.CreationDate
-		buck.Name = *bucket.Name
-		loc, _ := svc.GetBucketLocation(&s3.GetBucketLocationInput{Bucket: bucket.Name})
-		buck.Region = *loc.LocationConstraint
-		ad.Session.Config.Region = loc.LocationConstraint
-		svc = s3.New(ad.Session)
-		tags, _ := svc.GetBucketTagging(&s3.GetBucketTaggingInput{Bucket: bucket.Name})
-		tagset := make(map[string]string)
-		for _, tag := range tags.TagSet {
-			tagset[*tag.Key] = *tag.Value
+func ObjectListGCP(gcc *storage.Client, ctx context.Context, projectNumber string, bucket *model.MetaBucket) error {
+	fmt.Printf("\nbucket.Name: %v\n", bucket.Name)
+	objectIterator := gcc.Bucket(bucket.Name).Objects(ctx, nil)
+	fmt.Printf("\nobjectIterator: %v\n", objectIterator)
+
+	objectArray := make([]model.MetaObject, 1)
+	i := 0
+	for {
+		object, err := objectIterator.Next()
+		fmt.Printf("object: %v\n", object)
+		fmt.Print("AMIT:::Object Name " + object.Name + "\n")
+		i = i + 1
+		// go iterateObjects(i, object, svc, bucket.Name, objectArray)
+
+		if err == iterator.Done {
+			break
 		}
-		buck.BucketTags = tagset
-		objects, err := ad.ObjectList(ctx, &buck)
-		buck.Objects = objects
-		if err == nil {
-			// lock.Lock()
-			// defer lock.Unlock()
-			bucketArray = append(bucketArray, buck)
+		if err != nil {
+			return fmt.Errorf("Bucket(%q).Objects: %v", bucket, err)
 		}
+		fmt.Print("AMIT:::Object Name " + object.Name + "\n")
+		fmt.Printf("object: %v\n", object)
+
 	}
-	// wg.Wait()
-	return bucketArray, err
+	bucket.NumberOfObjects = int64(i)
+	bucket.Objects = objectArray
+	return nil
 }
 
-func (ad *AwsAdapter) ObjectList(ctx context.Context, bucket *model.MetaBucket) ([]model.MetaObject, error) {
-	svc := s3.New(ad.Session)
+func iterateBuckets(i int, gcc *storage.Client, ctx context.Context, projectNumber string, bucket *storage.BucketAttrs, bucketArray []model.MetaBucket) {
+	buck := model.MetaBucket{}
+	buck.CreationDate = &bucket.Created
+	buck.Name = bucket.Name
+	// loc, err := svc.GetBucketLocation(&s3.GetBucketLocationInput{Bucket: bucket.Name})
+	// if err != nil {
+	// 	log.Errorf("unable to get bucket location. failed with error: %v\n", err)
+	// } else {
+	// 	buck.Region = *loc.LocationConstraint
+	// }
+	// newSvc := s3.New(sess, aws.NewConfig().WithRegion(buck.Region))
+	// tags, err := newSvc.GetBucketTagging(&s3.GetBucketTaggingInput{Bucket: bucket.Name})
 
-	output, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: &bucket.Name})
+	// if err != nil && !strings.Contains(err.Error(), "NoSuchTagSet") {
+	// 	log.Errorf("unable to get bucket tags. failed with error: %v\n", err)
+	// } else {
+	// 	tagset := make(map[string]string)
+	// 	for _, tag := range tags.TagSet {
+	// 		tagset[*tag.Key] = *tag.Value
+	// 	}
+	// 	buck.BucketTags = tagset
+	// }
+	err := ObjectListGCP(gcc, ctx, projectNumber, &buck)
 	if err != nil {
-		return nil, err
+		fmt.Printf("error while collecting object metadata for bucket %v. failed with error: %v\n", buck.Name, err)
 	}
-	objectArray := make([]model.MetaObject, 0)
-	// var wg sync.WaitGroup
-	// var lock sync.Mutex
-	var numObjs, totSize int64 = 0, 0
-	for _, object := range output.Contents {
-		// wg.Add(1)
-		numObjs += 1
-		obj := model.MetaObject{}
-		obj.LastModifiedDate = object.LastModified
-		obj.ObjectName = *object.Key
-		obj.Size = *object.Size
-		totSize += obj.Size
-		obj.StorageClass = *object.StorageClass
-
-		meta, _ := svc.HeadObject(&s3.HeadObjectInput{Bucket: &bucket.Name, Key: object.Key})
-		if meta.ServerSideEncryption != nil {
-			obj.ServerSideEncryption = *meta.ServerSideEncryption
-		}
-		if meta.VersionId != nil {
-			obj.VersionId = *meta.VersionId
-		}
-		obj.ObjectType = *meta.ContentType
-		if meta.Expires != nil {
-			obj.ExpiresDate = *meta.Expires
-		}
-		if meta.ReplicationStatus != nil {
-			obj.ReplicationStatus = *meta.ReplicationStatus
-		}
-		// lock.Lock()
-		// defer lock.Unlock()
-		objectArray = append(objectArray, obj)
-		// wg.Done()
-	}
-	// wg.Wait()
-	bucket.NumberOfObjects = numObjs
-	bucket.TotalSize = totSize
-	return objectArray, err
+	bucketArray[i] = buck
 }
